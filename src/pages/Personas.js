f93cf30970780c9d1980ab551aef17919ce2028b
wild320 <wildesjaramillo@gmail.com>
2022-03-26 17:59:49 -0500

    fix-async

--- src/pages/Personas.js
+++ src/pages/Personas.js
@@ -1,31 +1,22 @@
 import  React, {useState,useEffect} from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import axios from 'axios';
 import {PERSONAS_CREADAS_ENDPOINT} from '../helpers/endpoints';
 import '@fontsource/roboto/300.css';
-
-
+import axios from 'axios';
+import Box from '@mui/material/Box';
 
 function Personas(){
-    const [listadoPersonas, setListadoPersonas] = useState([]);
-    const [fetching, setFeching] = useState(true);
+    const [data, setListadoPersonas] = useState([]);
 
-    useEffect(()=>{
-        axios.get(PERSONAS_CREADAS_ENDPOINT,{ 
-            headers: { 
-                'Access-Control-Allow-Origin' : '*',
-                'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-              }
-     })
-        .then(response => {               
-        setListadoPersonas(response.listadoPersonas);
-        setFeching(false);
-    }).catch(e => {
-        console.error(e);
-        setFeching(false);
-        })
-    }, []);
-
+    const fetchPersonas = async() => {
+       await axios.get(PERSONAS_CREADAS_ENDPOINT).then((response) => {
+                setListadoPersonas(response.data);
+            })
+      }
+      useEffect(()=>{
+        fetchPersonas();
+      },[]);
+      
     const columns = [   
         { field: 'id', headerName: 'ID', width: 50 },    
         { field: 'tipo_documento', headerName: 'Tipo Documento', width: 140 },
@@ -36,7 +27,7 @@ function Personas(){
      
      ];
 
-     const rows = listadoPersonas.map((row) => row({
+     const rows = data.map((row) =>({
          id: row.id,
          tipo_documento: row.tipo_documento,
          documento: row.documento,
@@ -44,23 +35,28 @@ function Personas(){
          apellidos: row.apellidos,
          hobbie: row.hobbie,
 
-     }))
+     }));
 
 
-    console.log(listadoPersonas)
-    return <div style={{height:100, width:'80%'}} >
-        <h2>Lista de Personas</h2>
-        <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-            checkboxSelection
-            />
-            {fetching}  
-             
-            {fetching  && <h3>no hay</h3>} 
-        </div>
+    console.log(data);
+    return(
+        <Box sx={{ display: 'flex', justifyContent: 'center', }}>
+            <Box  sx={{
+            width: '80%',
+            height: 350,
+            }}>
+                <h2>Lista de Personas</h2>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                    /> 
+            </Box>
+        </Box>
+    ) 
+        
 }
  
  export {Personas}
--- src/routes/App.js
+++ src/routes/App.js
@@ -14,7 +14,7 @@ import {BrowserRouter, Routes ,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">  
       
       { <header className="App-header">
         <Container>
